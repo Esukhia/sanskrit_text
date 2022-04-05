1626ba219d56131eb8402fd64ba6a6c63e85e52b
tenzin <ten13zin@gmail.com>
2022-04-05 12:05:08 +0530

    extracting Sanskrit words with kmean clustering

---
+++ extract_sanskrit_with_kmean_clustering.py
@@ -0,0 +1,90 @@
+import re
+from pathlib import Path
+from typing import Dict, List
+
+import numpy as np
+from botok.third_party.has_skrt_syl import is_skrt
+from sklearn.cluster import AgglomerativeClustering, KMeans
+
+
+def syl_tokenizer(string: str) -> List[str]:
+    chunks = re.split("(་|།།|།)", string)
+    syls = []
+    cur_syl = ""
+    for chunk in chunks:
+        if re.search("་|།།|།", chunk):
+            cur_syl += chunk
+            syls.append(cur_syl)
+            cur_syl = ""
+        else:
+            cur_syl += chunk
+    if cur_syl:
+        syls.append(cur_syl)
+    return syls
+
+
+def syls_to_skrt_vectors(syls: List[str]):
+    skrt_vector_idx_to_syls_idx = {}
+
+    skrt_syl_counter = 0
+    for idx, syl in enumerate(syls):
+        if is_skrt(syl):
+            skrt_vector_idx_to_syls_idx[skrt_syl_counter] = idx
+            skrt_syl_counter += 1
+
+    skrt_vectors = np.array([[i, 0] for i in skrt_vector_idx_to_syls_idx.values()])
+    return skrt_vectors, skrt_vector_idx_to_syls_idx
+
+
+def find_skrt_cluster_with_kmean(X):
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+    return list(kmeans.labels_)
+
+
+def find_skrt_cluster_with_agglo(X):
+    clustering = AgglomerativeClustering()
+    clustering.fit(X)
+    return list(clustering.labels_)
+
+
+def convert_cluster_to_skrt_words(
+    syls: List[str], labels: List[int], mapping: Dict[int, int]
+) -> List[str]:
+    reversed_lables = labels[::-1]
+    skrt_words_span = []
+    syl_idx = 0
+    while syl_idx <= len(labels) - 1:
+        label = labels[syl_idx]
+        label_end_idx = (len(labels) - 1) - reversed_lables.index(label)
+        skrt_words_span.append((mapping[syl_idx], mapping[label_end_idx]))
+        syl_idx = label_end_idx + 1
+
+    return ["".join(syls[start : end + 1]) for start, end in skrt_words_span]
+
+
+def extrack_skrt_words(text: str) -> List[str]:
+    syls = syl_tokenizer(text)
+    skrt_syls_vectors, vec_idx2syl_idx = syls_to_skrt_vectors(syls)
+    if len(skrt_syls_vectors) < 2:
+        return []
+    labels = find_skrt_cluster_with_kmean(skrt_syls_vectors)
+    skrt_words = convert_cluster_to_skrt_words(syls, labels, vec_idx2syl_idx)
+    return skrt_words
+
+
+def main():
+    print("[INFO]: Extracking Sankrit words...")
+    text_path = Path("./sanskrit_text")
+    skrt_words_fn = text_path / "skrt_words.txt"
+    skrt_words = []
+    for fn in text_path.iterdir():
+        for line in fn.read_text().splitlines():
+            skrt_words += extrack_skrt_words(line)
+        break
+    skrt_words_fn.write_text("\n".join(skrt_words))
+    print(f"[INFO] {len(skrt_words)} Sanskrit words found")
+    print("[INFO] saved at {skrt_words_fn}")
+
+
+if __name__ == "__main__":
+    main()
